0fdc529dc0a83597a229bdbc387399106576175f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-05 14:52:31 -0400

    Timetracking: Add seeds

--- timetracker/db/seeds.rb
+++ timetracker/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating three projects"
+projectA = Project.create(name: "Ironhack", description: "Coding bootcamp")
+projectB = Project.create(name: "Papa Johns", description: "Decent chain for pizza")
+projectC = Project.create(name: "Taco Bell")
+
+
+puts "done"
+
+
+# rails db:seed
