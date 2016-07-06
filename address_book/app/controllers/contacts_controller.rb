544ca8850240cfa0b0bdcddae7a4617159d7ffaa
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-06 09:16:00 -0400

    Address Book: Create new contacts

--- address_book/app/controllers/contacts_controller.rb
+++ address_book/app/controllers/contacts_controller.rb
@@ -3,4 +3,20 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
     render "index"
   end
+
+  def new
+    render "new"
+  end
+
+  def create
+    contact = Contact.new(
+      :name => params[:contact][:name],
+      :address => params[:contact][:address],
+      :phone_number => params[:contact][:phone_number],
+      :email_address => params[:contact][:email_address])
+
+    contact.save
+
+    redirect_to("/contacts")
+  end
 end
--- address_book/config/routes.rb
+++ address_book/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "/contacts" => "contacts#index"
 
+  get "/contacts/new" => "contacts#new"
+  post "/contacts" => "contacts#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
