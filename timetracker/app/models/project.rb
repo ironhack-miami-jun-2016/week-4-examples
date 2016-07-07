5722bd078d34a53dbf0ad2249af842c705b955cc
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-07 10:53:39 -0400

    Timetracker: Add clean_old method

--- timetracker/app/models/project.rb
+++ timetracker/app/models/project.rb
@@ -1,6 +1,14 @@
 class Project < ApplicationRecord
   has_many :time_entries
+
+  def self.clean_old
+    old_projects = Project.where("created_at < ?", 1.week.ago)
+    old_projects.destroy_all
+  end
 end
+
+# In the rails console try:
+  # Project.clean_old
 
 
 # the_project = Project.find_by(id: 1)
